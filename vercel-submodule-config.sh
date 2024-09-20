9b409d20703b4afe8d3ed7d872c767ff5bfde81a
cooper <pbg0205@naver.com>
2024-09-20 18:35:37 +0200

    build: modify git pull submodule

--- vercel-submodule-config.sh
+++ vercel-submodule-config.sh
@@ -8,27 +8,16 @@ fi
 
 mkdir data
 
-output=`git submodule status --recursive` # get submodule info
-no_prefix=${output#*-} # get rid of the prefix
-COMMIT=${no_prefix% *} # get rid of the suffix
-
-# set up an empty temporary work directory
-rm -rf tmp_submodule || true # remove the tmp folder if exists
-mkdir tmp_submodule # create the tmp folder
-cd tmp_submodule # go into the tmp folder
-
-# checkout the current submodule commit
-git init # initialise empty repo
-git remote add origin https://$GITHUB_ACCESS_TOKEN@$SUBMODULE_GITHUB # add origin of the submodule
+git clone https://$GITHUB_ACCESS_TOKEN@$SUBMODULE_GITHUB # add origin of the submodule
 git fetch --depth=1 origin $COMMIT # fetch only the required version
 git checkout $COMMIT # checkout on the right commit
 
 cd .. # go folder up
 echo "$(pwd)"
 
-rm -rf ./tmp_submodule/.git # remove .git
-mv ./tmp_submodule/* $SUBMODULE_PATH/ # move the submodule to the submodule path
+rm -rf ./ssa-data/.git # remove .git
+mv ./ssa-data/* $SUBMODULE_PATH/ # move the submodule to the submodule path
 echo "$(ls)"
 
 # clean up
-rm -rf tmp_submodule # remove the tmp folder
+rm -rf ssa-data # remove the tmp folder
