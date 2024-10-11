1ee58f5100af03c3b50bf8d56a72aea601d78dce
cooper <pbg0205@naver.com>
2024-10-11 10:42:20 +0200

    style: 문제 선택지 위치 변경

--- src/components/ChoiceList.tsx
+++ src/components/ChoiceList.tsx
@@ -4,7 +4,7 @@ type ChoiceListProps = {
 
 export function ChoiceList({ choices }: ChoiceListProps) {
   return (
-    <ul className="pl-6 mb-6">
+    <ul className="mb-6">
       {choices.map((choice: string, index: number) => (
         <li key={index} className="mb-2">
           {choice}
