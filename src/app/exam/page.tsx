bd68c21f671593a1f049d1fb437187ba76a31b1d
cooper <pbg0205@naver.com>
2024-11-16 17:14:11 +0100

    refactor: ResultModal 닫기 버튼 제거

--- src/app/exam/page.tsx
+++ src/app/exam/page.tsx
@@ -139,7 +139,6 @@ export default function ExamPage() {
           <ResultModal
             correctAnswers={correctAnswers}
             selectedAnswers={selectedAnswers}
-            onClose={() => setShowModal(false)}
           />
         )}
       </div>
