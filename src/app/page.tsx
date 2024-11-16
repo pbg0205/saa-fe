aebb71d7440a3135f80bec1b795402b2a4d8b29d
cooper <pbg0205@naver.com>
2024-11-16 17:35:08 +0100

    feat: 메인 메뉴 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <>
-      <h1>메인 페이지</h1>
+      <h2 className="text-2xl font-bold">2. 메뉴 설명</h2>
+      <ul className="text-lg">
+        <li>KO - 한국어 문제</li>
+        <li>EN - 영어 문제</li>
+        <li>ANSWERS - 문제 정답</li>
+        <li>RANDOM - 랜덤 문제</li>
+        <li>EXAM - 시험 모드</li>
+      </ul>
     </>
   );
 }
