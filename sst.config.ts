c71009f69a4d9d71ec5266af45261bcd4a2c5745
cooper <pbg0205@naver.com>
2024-11-27 18:20:37 +0900

    fix: 빌드 에러 이슈 코드 변경

--- sst.config.ts
+++ sst.config.ts
@@ -1,15 +1,15 @@
-import { SSTConfig, SSTApp, SSTStack } from "sst";
+import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
 export default {
-  config(_input: any) {
+  config(_input) {
     return {
       name: "ssa-fe",
       region: "us-east-1",
     };
   },
-  stacks(app: SSTApp) {
-    app.stack(function Site({ stack }: { stack: SSTStack }) {
+  stacks(app) {
+    app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "site", {
         customDomain: {
           domainName: stack.stage === "prod" ? "ssa-fe.com" : "dev.ssa-fe.com",
